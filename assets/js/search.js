4bd22510cf5769c920e2d833684d43c47798de7e
heny <cchuam@qq.com>
2024-09-06 12:00:28 +0800

    feat: 调整公共数据

--- assets/js/search.js
+++ assets/js/search.js
@@ -3,6 +3,7 @@ function searchData () {
   let self = null;
 
   return {
+    list: searchList,
     content: '',
     currentSearchCateGory: '',
     currentSearchData: {},
@@ -54,84 +55,6 @@ function searchData () {
         self.$refs.searchInput.focus();
       }
     },
-
-    list: [
-      {
-        name: '搜索',
-        key: 'search',
-        children: [
-          {
-            name: '本地',
-            key: 'local',
-            icon: 'icon-bendi'
-          },
-          {
-            name: '百度',
-            key: 'baidu',
-            icon: 'icon-baidu',
-            url: 'https://www.baidu.com/s?ie=UTF-8&wd=%s'
-          },
-          {
-            name: '谷歌',
-            key: 'google',
-            icon: 'icon-google',
-            url: 'https://www.google.com/search?q=%s'
-          },
-          {
-            name: 'Bing',
-            key: 'bing',
-            icon: 'icon-Bing',
-            url: 'https://www.bing.com/search?q=fdsa'
-          },
-          {
-            name: '开发',
-            key: 'kaifa',
-            url: 'https://kaifa.baidu.com/searchPage?wd=%s'
-          },
-        ]
-      },
-      {
-        name: '社交',
-        key: 'social',
-        children: [
-          {
-            name: '掘金',
-            key: 'juejin',
-            icon: 'icon-juejin',
-            url: 'https://juejin.cn/search?query=%s&type=0'
-          },
-        ]
-      },
-      {
-        name: '开发',
-        key: 'dev',
-        children: [
-          {
-            name: 'GitHub',
-            key: 'github',
-            icon: 'icon-github-fill',
-            url: 'https://github.com/search?q=%s&ref=opensearch'
-          },
-          {
-            name: 'npmjs',
-            key: 'npmjs',
-            icon: 'icon-npmjs-fill',
-            url: 'https://www.npmjs.com/search?q=%s'
-          },
-          {
-            name: '码云',
-            key: 'gitee',
-            icon: 'icon-gitee-fill-round',
-            url: 'https://gitee.com/search?utf8=%E2%9C%93&type=&fork_filter=on&q=%s'
-          },
-          {
-            name: 'CodePen',
-            key: 'codepen',
-            url: 'https://codepen.io/search/pens?q=%s'
-          },
-        ]
-      }
-    ],
 
     get listItem () {
       return this.list.find(item => item.key === this.currentSearchCateGory).children
