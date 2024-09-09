bca538f571f3312fa06672c03e5eb13844eb1fb1
heny <cchuam@qq.com>
2024-09-09 10:03:03 +0800

    feat: 更新社交项目

--- publish.sh
+++ publish.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# 如果任何命令失败，立即退出脚本
+set -e
+
 # 检查是否提供了提交信息
 if [ $# -eq 0 ]; then
     echo "请提供提交信息"
@@ -11,21 +14,6 @@ commit_message="$1"
 
 # 获取当前时间戳
 timestamp=$(date +%Y%m%d%H%M%S)
-
-# 更新 JS 文件
-for file in assets/js/*.js; do
-    update_file "$file" "js"
-done
-
-# 更新 CSS 文件
-for file in assets/css/*.css; do
-    update_file "$file" "css"
-done
-
-# 更新 SCSS 文件
-for file in assets/scss/*.scss; do
-    update_file "$file" "scss"
-done
 
 # 文件更新函数
 update_file() {
@@ -39,11 +27,26 @@ update_file() {
         base_name="${filename%.$ext}"
         new_name=$(dirname "$file")/${base_name}.${timestamp}.$ext
     fi
-    mv "$file" "$new_name"
-    sed -i "s|$filename|$(basename "$new_name")|g" index.html
+    mv "$file" "$new_name" || { echo "移动文件失败: $file"; exit 1; }
+    sed -i "s|$filename|$(basename "$new_name")|g" index.html || { echo "更新 index.html 失败"; exit 1; }
 }
 
+# 更新 JS 文件
+for file in assets/js/*.js; do
+    update_file "$file" "js"
+done
+
+# 更新 CSS 文件
+for file in assets/css/*.css; do
+    update_file "$file" "css"
+done
+
+# 更新 SCSS 文件
+for file in assets/css/*.scss; do
+    update_file "$file" "scss"
+done
+
 # 提交更改
-git-auto push -m "$commit_message"
+git-auto push -m "$commit_message" || { echo "Git 提交失败"; exit 1; }
 
 echo "文件已更新并提交，提交信息：$commit_message"
